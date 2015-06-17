88d3bc43c0c743bc53a477831d903bef5b0d9916
FidaAlSughayer <f.a.sghyr@gmail.com>
2015-06-17 13:40:10 -0700

    Landing using images

--- Final/core/src/uw/haptic2015/screens/LandingScreen.java
+++ Final/core/src/uw/haptic2015/screens/LandingScreen.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.sun.prism.Texture;
 
 import uw.haptic2015.Main;
 
@@ -18,8 +19,8 @@ public class LandingScreen implements Screen {
 
     OrthographicCamera camera;
     SpriteBatch batch;
-    Sprite sprite;
-    Texture img;
+    Sprite sprite, title, sub1, sub2, sub3, shutdown;
+    Texture img, title_txt, sub1_txt, sub2_txt, sub3_txt, shutdown_txt;
     TextureRegion region;
     float screenWidth, screenHeight;
     Main main;
@@ -38,12 +39,22 @@ public class LandingScreen implements Screen {
         batch = new SpriteBatch();
 
         img = new Texture("grey_grid_landscape.png");
+        title_txt = new Texture("title.PNG");
+        sub1_txt = new Texture("subtitle1.PNG");
+        sub2_txt = new Texture("subtitle2.PNG");
+        sub3_txt = new Texture("subtitle3.PNG");
+        shutdown_txt = new Texture("shutdown.PNG"");
         //img.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
 
         //region = new TextureRegion(img, 0, 0, 800, 420);
 
         //sprite = new Sprite(region);
         sprite = new Sprite(img);
+        title = new Sprite(title_txt);
+        sub1 = new Sprite(sub1_txt);
+        sub2 = new Sprite(sub2_txt);
+        sub3 = new Sprite(sub3_txt);
+        shutdown = new Sprite(shutdown_txt);
         //sprite.setSize(1f, 1f * sprite.getHeight()/sprite.getWidth());
         //sprite.scale(1f);
     }
@@ -59,6 +70,12 @@ public class LandingScreen implements Screen {
 
         //sprite.draw(batch);
         batch.draw(sprite, 0, 0);
+        batch.draw(title, 0, 0);
+        batch.draw(sub1, 0, 0);
+        batch.draw(sub2, 0, 0);
+        batch.draw(sub3, 0, 0);
+        batch.draw(shutdown, 0, 0);
+
 
         batch.end();
 
